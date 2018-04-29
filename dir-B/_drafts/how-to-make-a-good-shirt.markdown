da3cc621cd25c9752d75f307939468855effcb2c
Harper Reed <harper@nata2.org>
2018-04-28 21:58:28 -0500

    olllllddd posts

--- dir-B/_drafts/how-to-make-a-good-shirt.markdown
+++
@@ -1,6 +0,0 @@
---- 
-layout: post
-title: "How to make a good shirt. Pro-tips. "
-date: 2010-08-24 00:46:42 -06:00
---- 
-
--- dir-B/_import/dos2unix.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-tmpfile="/tmp/$$"
-if [ $# -ne 1 ] ; then
-echo "USAGE $0 dosfile"
-exit 4
-fi
-tr -d '\015\032' < $1 > $tmpfile && mv $tmpfile $1
